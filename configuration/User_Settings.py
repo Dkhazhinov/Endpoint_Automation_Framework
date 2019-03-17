547487b1a6652a00ca4231733b7e5707f737012c
Dmitry Khazhinov <dkhazhinov@gmail.com>
2019-03-17 00:06:49 -0700

    User Settings file

---
+++ configuration/User_Settings.py
@@ -0,0 +1,13 @@
+# ----- REQUIRED -----
+# 1 : Choose EnvToRunTestsOn
+# 2 : Choose WebDriverBrowser
+
+# Select Environmnet to run auto_test in
+EnvToRunTestsOn = 'Production'
+# EnvToRunTestsOn = 'Staging'
+# EnvToRunTestsOn = 'Dev'
+
+# Select a browser
+# WebDriverBrowser = 'Chrome'
+# WebDriverBrowser = 'FireFox'
+WebDriverBrowser = 'Safari'
